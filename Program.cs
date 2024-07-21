8f3cdfa4cdc7a42d78f2b996d652e06cc4acfc20
svetlana <svetlana.grigoryan08@gmail.com>
2024-07-21 20:53:29 +0400

    alabala

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] arr = new[] { "Hayastan", "Svetik", "muk", "jut" };
+int size = arr.Length;
+string [] arr2 = new string [size];
+int j = 0;
+for (int i = 0; i < size; i++)
+{
+    if (arr[i].Length <= 3)
+    {
+        arr2[j] = arr[i];
+        Console.Write($"{arr2[j]}, ");
+        j++;
+    }
+}
